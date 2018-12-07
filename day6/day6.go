395381af06782ded838acdf38d05be72c6d89400
hugsjo <hugo.sjoberg@zenuity.com>
2018-12-07 14:33:36 +0100

    Solutions day6

--- day6/day6.go
+++ day6/day6.go
@@ -18,24 +18,20 @@ func abs(x int) int {
 }
 
 func distance(pc, c cord) int {
-	dist := abs(pc.x-c.x) + abs(pc.y-c.y)
-	return dist
+	return abs(pc.x-c.x) + abs(pc.y-c.y)
 }
 
-func shortestDist(pc cord, cords map[cord]int) int {
-	id := 0
-	var dist, tempDist int
-	for c, i := range cords {
+func shortestDist(pc cord, cords map[int]cord) int {
+	dist := 1000
+	var tempDist, id int
+	for i, c := range cords {
 		if pc.x == c.x && pc.y == c.y {
 			return i
 		}
 		tempDist = distance(pc, c)
-		if tempDist == 0 {
-			return i
-		} else if dist == 0 {
-			dist = tempDist
-			id = i
 
+		if tempDist == dist {
+			id = 0
 		} else if dist > tempDist {
 			dist = tempDist
 			id = i
@@ -44,10 +40,8 @@ func shortestDist(pc cord, cords map[cord]int) int {
 	return id
 }
 
-func findInfinite(pc cord) bool {
-	if pc.x == 0 || pc.x == 1000 {
-		return true
-	} else if pc.y == 0 || pc.y == 1000 {
+func findInfinite(pc cord, minX, maxX, minY, maxY int) bool {
+	if pc.x == minX || pc.x == maxX || pc.y == minY || pc.y == maxY {
 		return true
 	}
 	return false
@@ -69,41 +63,73 @@ func main() {
 
 	id := 1
 	system := make(map[cord]int)
-	cords := make(map[cord]int)
+	cords := make(map[int]cord)
 	infinity := make(map[int]bool)
-
+	var minX, minY, maxX, maxY = 1000, 1000, -1000, -1000
 	for scan.Scan() {
 		line := scan.Text()
 		var co cord
 		fmt.Sscanf(line, "%d, %d", &co.x, &co.y)
-		cords[co] = id
+		cords[id] = co
 		infinity[id] = false
 		id++
+		if co.x < minX {
+			minX = co.x
+		} else if co.x > maxX {
+			maxX = co.x
+		}
+		if co.y < minY {
+			minY = co.y
+		} else if co.y > maxY {
+			maxY = co.y
+		}
 	}
 
-	for x := 0; x < 1000; x++ {
-		for y := 0; y < 1000; y++ {
+	area := make(map[int]int)
+	for x := minX; x <= maxX; x++ {
+		for y := -minY; y <= maxY; y++ {
 			var co cord
 			co.x = x
 			co.y = y
+			if x == 118 && y == 274 {
+			}
 			id = shortestDist(co, cords)
+			area[id]++
 			system[co] = id
 			if infinity[id] == false {
-				infinity[id] = findInfinite(co)
+				infinity[id] = findInfinite(co, minX, maxX, minY, maxY)
 			}
 		}
 	}
-	area := 0
-	for id, inf := range infinity {
-		if inf == false {
-			tempArea := calArea(system, id)
-			if area > tempArea {
-				area = tempArea
-			} else if area == 0 {
-				area = tempArea
+	maxArea := 0
+	for id, v := range area {
+		if infinity[id] == false {
+			if v > maxArea {
+				maxArea = v
 			}
 		}
 	}
-	fmt.Println(area)
+	fmt.Println("Part1 :")
+	fmt.Println(maxArea)
 
+	m := make(map[cord]int)
+	for x := minX; x <= maxX; x++ {
+		for y := -minY; y <= maxY; y++ {
+			sum := 0
+			var c cord
+			c.x = x
+			c.y = y
+			for _, co := range cords {
+				sum += distance(c, co)
+			}
+			m[c] = sum
+		}
+	}
+	maxArea = 0
+	for _, d := range m {
+		if d < 10000 {
+			maxArea++
+		}
+	}
+	fmt.Println(maxArea)
 }
